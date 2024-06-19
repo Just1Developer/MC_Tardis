93a4f7ea79fb0883316ccc3d79ea3b0da40ba325
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-06-19 22:16:00 +0200

    Removed some debug messages

--- src/main/java/net/justonedev/mc/tardisplugin/tardis/TardisEvents.java
+++ src/main/java/net/justonedev/mc/tardisplugin/tardis/TardisEvents.java
@@ -1,7 +1,6 @@
 package net.justonedev.mc.tardisplugin.tardis;
 
 import net.justonedev.mc.tardisplugin.TardisPlugin;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -9,49 +8,31 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
-import java.util.UUID;
-
 public class TardisEvents implements Listener {
     
     @EventHandler
     public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) {
-        Bukkit.broadcastMessage("Click event triggered");
         Entity entity = e.getRightClicked();
-        Bukkit.broadcastMessage("Interact 1");
         if (!isTardisComponent(entity)) return;
-        Bukkit.broadcastMessage("Interact 2");
         
         Tardis tardis = TardisPlugin.getTardisByEntityUUID(entity.getUniqueId());
-        Bukkit.broadcastMessage("§cTardis: " + tardis);
-        Bukkit.broadcastMessage("§cContains: " + TardisPlugin.singleton.tardisesByEntityUUID.containsKey(entity.getUniqueId()));
-        Bukkit.broadcastMessage("§cSize: " + TardisPlugin.singleton.tardisesByEntityUUID.size());
-        for (UUID uuid : TardisPlugin.singleton.tardisesByEntityUUID.keySet()) {
-            Bukkit.broadcastMessage("§9UUID: " + uuid);
-        }
         if (tardis == null) return;
         e.setCancelled(true);   // Event would probably be change equipment
-        Bukkit.broadcastMessage("Interact 3");
 
         tardis.enter(e.getPlayer());
-        Bukkit.broadcastMessage("Interact 4 (entered)");
     }
     
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-        Bukkit.broadcastMessage("Damage event triggered");
         Entity entity = e.getEntity();
         if (!isTardisComponent(entity)) return;
         e.setCancelled(true);
-        Bukkit.broadcastMessage("Tardis is immortal");
     }
     
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     private static boolean isTardisComponent(Entity entity) {
-        Bukkit.broadcastMessage("§eCheck: Type: " + entity.getType());
         if (entity.getType() != EntityType.ARMOR_STAND) return false;
-        // Check if tardis door:
-        Bukkit.broadcastMessage("§eCheck: hasCustomName: " + (entity.getCustomName() != null));
         if (entity.getCustomName() == null) return false;
-        Bukkit.broadcastMessage("§eCheck: customName: " + entity.getCustomName() + " - condition: " + entity.getCustomName().startsWith("tardis"));
         return entity.getCustomName().startsWith("tardis");
     }
 
