1fc5d2414ee3e3dbb46576f27da85998f84361aa
Just1Developer <weisserbenjamin98@gmail.com>
2024-06-21 13:31:38 +0200

    [TRANSFER-COMMIT] minor development

--- src/main/java/net/justonedev/mc/tardisplugin/schematics/Cluster.java
+++ src/main/java/net/justonedev/mc/tardisplugin/schematics/Cluster.java
@@ -2,11 +2,13 @@ package net.justonedev.mc.tardisplugin.schematics;
 
 import net.justonedev.mc.tardisplugin.TardisPlugin;
 import org.bukkit.Material;
+import org.bukkit.util.Vector;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +17,9 @@ import java.util.Set;
  * A collection of Quaders with the same material but different attributes.
  */
 public class Cluster {
+
+    static final int SIZE_UPPERBOUND_16_BIT = 0xFF00;
+    static final int SIZE_UPPERBOUND_24_BIT = 0xFF0000;
 
     Material material;
     List<Quader> quaders;
@@ -30,6 +35,9 @@ public class Cluster {
         else material = null;
     }
 
+    // First byte: first bit gives format: 0 => 16-bit, 1 => 24-bit
+    // Next 7 bit => amount of following ascii characters
+
     byte[] encode() {
         // 1. Byte that list how many letters (bytes) in ASCII the type will be.
         // 2. Then, the type with 1 ASCII letter per byte.
@@ -41,11 +49,20 @@ public class Cluster {
             return new byte[0];
         }
 
+        boolean need24Bit = false;
+        for (Quader quader : quaders) {
+            // Check if any of the coordinates are >= SIZE_UPPERBOUND_16_BIT
+            // If so, mark we need 24 bit
+            // If any of the coordinates are >= SIZE_UPPERBOUND_24_BIT, return empty array and print error.
+        }
+
         String materialString = material.name();
-        bytes.add((byte) materialString.length());
+        bytes.add((byte) (materialString.length() & 0x7F | (need24Bit ? 0x80 : 0x00)));
         for (int _c = 0; _c < materialString.length(); _c++) {
             bytes.add((byte) materialString.charAt(_c));
         }
+
+        HashMap<Vector, List<Quader>> differentShapes;
 
         byte[] byteArray = new byte[bytes.size()];
         int index = 0;
